dc84279b600209d912bdacc1bede7d1ad83e5439
Ricky White <ricky@migusgroup.com>
2023-09-20 08:42:29 -0400

    Parse new args for verification

--- cmd/ssh-sign/main.go
+++ cmd/ssh-sign/main.go
@@ -36,62 +36,78 @@ func main() {
 	var action string
 	var namespace string
 	var sshUID string
+	var signatureFile string
+	var timestamp string
 
-	flag.StringVar(&action, "Y", "", "Action to perform") // Only 'sign' is currently supported.
-	flag.StringVar(&namespace, "n", "", "Namespace")      // Only 'git' is supported.
+	flag.StringVar(&action, "Y", "", "Action to perform") 
+	flag.StringVar(&namespace, "n", "", "Namespace")      
 	flag.StringVar(&sshUID, "f", "", "SSH Key UID")
+	flag.StringVar(&signatureFile, "s", "", "Signature file for verificaton")
+	flag.StringVar(&timestamp, "Overify-time", "", "TODO")
 	flag.Parse()
 
-	if len(flag.Args()) == 0 {
+	if len(os.Args) == 0 {
 		fmt.Println("This program is not intended to be run directly. It is called by git when signing commits.")
 		os.Exit(1)
 	}
+
+	// Only the 'git' namespace is supported.
 	if namespace != "git" {
 		fmt.Println("Only the 'git' namespace is supported.")
 		os.Exit(1)
 	}
-	if action != "sign" {
-		fmt.Println("Only the 'sign' action is supported.")
-		os.Exit(1)
-	}
 
-	// `flag.Args` returns the non-flag arguments, only. In this case, the 
-	// first and only argument should be the path to the file that contains the 
-	// commit data.
-	commitToSign := flag.Args()[0]
+	if action == "sign" {
+		// `flag.Args` returns the non-flag arguments, only. In this case, the 
+		// first and only argument should be the path to the file that contains 
+		// the commit data.
+		commitToSign := flag.Args()[0]
+		if commitToSign == "" {
+			fmt.Println("No commit file specified.")
+			os.Exit(1)
+		}
+	
+		privateKey, err := vault.FetchPrivateKey(sshUID)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	
+		file, err := os.Open(commitToSign)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	
+		// To ensure that git can read the final signature file, we capture the
+		// file permissions of the commit file to ensure the signature file has the
+		// same permissions.
+		fileinfo, err := os.Stat(commitToSign)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fileMode := fileinfo.Mode()
+	
+		sig, err := sign.SignCommit(privateKey, file)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	
+		if err := os.WriteFile(fmt.Sprintf("%s.sig", commitToSign), sig, fileMode); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		} else {
+			os.Exit(0)
+		}
+	} else if action == "check-novalidate" { 
+		// -Y check-novalidate -n git -s C:\Users\RICKYW~1\AppData\Local\Temp/.git_vtag_tmpudh6g6 -Overify-time=20230920083515
 
-	privateKey, err := vault.FetchPrivateKey(sshUID)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	file, err := os.Open(commitToSign)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// To ensure that git can read the final signature file, we capture the
-	// file permissions of the commit file to ensure the signature file has the
-	// same permissions.
-	fileinfo, err := os.Stat(commitToSign)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fileMode := fileinfo.Mode()
-
-	sig, err := sign.SignCommit(privateKey, file)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if err := os.WriteFile(fmt.Sprintf("%s.sig", commitToSign), sig, fileMode); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
 	} else {
-		os.Exit(0)
+		fmt.Println("Unsupported action. Only 'sign' and 'check-novalidate' are supported")
+		os.Exit(1)
 	}
 }
+
+
