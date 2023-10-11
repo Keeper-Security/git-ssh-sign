12f544625b893e710c3b2c27a8bc5efa95a8ea86
Ricky White <ricky@migusgroup.com>
2023-10-11 12:10:33 -0400

    Added more unit tests

--- internal/verify/verify_test.go
+++ internal/verify/verify_test.go
@@ -2,6 +2,10 @@ package verify
 
 import (
 	"bytes"
+	"encoding/pem"
+	"fmt"
+	"os"
+	"reflect"
 	"testing"
 
 	"github.com/Keeper-Security/git-ssh-sign/internal/sign"
@@ -20,7 +24,7 @@ lenrm3hZ4B7DdcB/4/H+AAAAEHRlc3RAZXhhbXBsZS5jb20BAgMEBQ==
 -----END OPENSSH PRIVATE KEY-----		
 `
 
-	ed25519PublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQvSrBv28KLAjYO7pD91prhlenrm3hZ4B7DdcB/4/H+ test@example.com"
+	ed25519PublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQvSrBv28KLAjYO7pD91prhlenrm3hZ4B7DdcB/4/H+"
 
 	// The following value was generated using the following command:
 	// 		ssh-keygen -C test@example -f test_key
@@ -64,9 +68,81 @@ MQUjv26NIZPFqtAAAADHRlc3RAZXhhbXBsZQECAwQFBg==
 -----END OPENSSH PRIVATE KEY-----
 `
 
-	rsaPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD8QxHgdYL7YQcy7HMsYn/JfFAfwJWNq5nCkkgfko7bA+NQeQcNzLhHIoz5TfCHux5Kb+H+mbb4mYZDY0eT2zgy5QbZkJpggeFw6mdT2n2Dgvof/gJULsDBh6ZbktRTlwkQXDg0OJT/W2CCo5J+5DoXaVczw68OmTd72j08/p56BsbXJK7RUpUK+m3gmhfmMQl7M5QCVVEe22PpXu7AMV6lmqym6cqcqMrman2+szcAvhqO+TNZBKI3zI8RXZHIHFSSQMlXMBqShm8IkynmRVy8kFy9K7tkjNoGlfiD/yi73ChaGZLhx2d8/cFm5FgRzEbA45fOVC0DMeNKpRoFejYZHrekwisAcIoZ2G2buxa6hzPxAC24cZ5F/eEeVmn+Lj8tWSPRR7p7AEMWnNqt0gNhY6wv+iAwkScvXRSBKwvnOQIXKGz2iYuRyhjuAkvrJDfpOw58bNILaTRzeRQAbR5UnsURN4nIKOg5/ioPkR85c4Dr31/DmAOTttdw4Sne0E= test@example"
+	rsaPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD8QxHgdYL7YQcy7HMsYn/JfFAfwJWNq5nCkkgfko7bA+NQeQcNzLhHIoz5TfCHux5Kb+H+mbb4mYZDY0eT2zgy5QbZkJpggeFw6mdT2n2Dgvof/gJULsDBh6ZbktRTlwkQXDg0OJT/W2CCo5J+5DoXaVczw68OmTd72j08/p56BsbXJK7RUpUK+m3gmhfmMQl7M5QCVVEe22PpXu7AMV6lmqym6cqcqMrman2+szcAvhqO+TNZBKI3zI8RXZHIHFSSQMlXMBqShm8IkynmRVy8kFy9K7tkjNoGlfiD/yi73ChaGZLhx2d8/cFm5FgRzEbA45fOVC0DMeNKpRoFejYZHrekwisAcIoZ2G2buxa6hzPxAC24cZ5F/eEeVmn+Lj8tWSPRR7p7AEMWnNqt0gNhY6wv+iAwkScvXRSBKwvnOQIXKGz2iYuRyhjuAkvrJDfpOw58bNILaTRzeRQAbR5UnsURN4nIKOg5/ioPkR85c4Dr31/DmAOTttdw4Sne0E="
 
 )
+
+var (
+	allowedSigners = []AllowedSigner{
+		{
+			Email: "test@example.com",
+			PublicKey: ed25519PublicKey,
+		},
+		{
+			Email: "test@example.com",
+			PublicKey: rsaPublicKey,
+		},
+	}
+)
+
+func TestFindMatchingPrincipals(t *testing.T) {
+	principal, _, _, _, err := ssh.ParseAuthorizedKey([]byte(allowedSigners[0].PublicKey))
+	if err != nil {
+		t.Fatalf("Failed to parse test principal: %v", err)
+	}
+	
+	sig := &Signature{
+        PublicKey: principal,
+    }
+
+    // Find matching principals
+    matchingPrincipals, err := FindMatchingPrincipals(allowedSigners, sig)
+    if err != nil {
+        t.Fatalf("FindMatchingPrincipals returned an error: %v", err)
+    }
+
+    // Check that the correct principals were found
+    expectedPrincipals := []string{allowedSigners[0].PublicKey}
+    if !reflect.DeepEqual(matchingPrincipals, expectedPrincipals) {
+        t.Errorf("FindMatchingPrincipals returned %v, expected %v", matchingPrincipals, expectedPrincipals)
+    }
+}
+
+func TestGetAllowedSigners(t *testing.T) {
+    // Create a test file
+    testFile := "testfile.txt"
+    f, err := os.Create(testFile)
+    if err != nil {
+		t.Fatalf("Failed to create test file: %v", err)
+    }
+    fmt.Fprintln(f, allowedSigners[0].Email, allowedSigners[0].PublicKey)
+    fmt.Fprintln(f, allowedSigners[1].Email, allowedSigners[1].PublicKey)
+	defer f.Close()
+	defer os.Remove(testFile)
+
+    // Get the AllowedSigners from the test file
+    as, err := GetAllowedSigners(testFile)
+    if err != nil {
+        t.Fatalf("GetAllowedSigners returned an error: %v", err)
+    }
+
+    if !reflect.DeepEqual(as, allowedSigners) {
+        t.Errorf("GetAllowedSigners returned %v, expected %v", as, allowedSigners)
+    }
+}
+
+func TestVerifyFingerprints(t *testing.T) {
+    // Create a test principal and public key
+    principal := []byte(ed25519PublicKey)
+    pubKey, _, _, _, err := ssh.ParseAuthorizedKey(principal)
+    if err != nil {
+        t.Fatalf("Failed to parse test principal: %v", err)
+    }
+
+    if err := VerifyFingerprints(principal, pubKey); err != nil {
+        t.Errorf("VerifyFingerprints returned an error: %v", err)
+    }
+}
 
 
 func TestVerifySignature(t *testing.T) {
@@ -121,6 +197,188 @@ func TestVerifySignature(t *testing.T) {
 			if err := VerifyFingerprints([]byte(otherSSHPublicKey), decodedSignature.PublicKey); err == nil {
 				t.Error("expected error!")
 			}
+		})
+	}
+
+}
+
+
+func TestValidDecode(t *testing.T) {
+	data := []byte("Hello, decode function!")
+
+	for _, tt := range []struct {
+		name string
+		pub  string
+		priv string
+	}{
+		{
+			name: "rsa",
+			pub:  rsaPublicKey,
+			priv: rsaPrivateKey,
+		},
+		{
+			name: "ed25519",
+			pub:  ed25519PublicKey,
+			priv: ed25519PrivateKey,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+
+			s, err := ssh.ParsePrivateKey([]byte(tt.priv))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			as, ok := s.(ssh.AlgorithmSigner)
+			if !ok {
+				t.Fatal(err)
+			}
+
+			signature, err := sign.NewSignature(as, bytes.NewReader(data))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Check that Decode returns no error when given a valid signature
+			_, err = Decode(sign.Armor(signature, s.PublicKey()))
+			if err != nil {
+				t.Fatalf("Decode returned an error: %v", err)
+			}
+		})
+	}
+
+}
+
+func TestInvalidDecode(t *testing.T) {
+	dataFile := "invalid-decode.txt"
+	f, err := os.Create(dataFile)
+	if err != nil {
+		t.Fatalf("Failed to create test file: %v", err)
+	}
+	fmt.Fprintln(f, "Hello, decode function!")
+	defer f.Close()
+	defer os.Remove(dataFile)
+
+	for _, tt := range []struct {
+		name string
+		pub  string
+		priv string
+	}{
+		{
+			name: "rsa",
+			pub:  rsaPublicKey,
+			priv: rsaPrivateKey,
+		},
+		{
+			name: "ed25519",
+			pub:  ed25519PublicKey,
+			priv: ed25519PrivateKey,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+
+			s, err := ssh.ParsePrivateKey([]byte(tt.priv))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			as, ok := s.(ssh.AlgorithmSigner)
+			if !ok {
+				t.Fatal(err)
+			}
+
+			sig, err := sign.NewSignature(as, f)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Wrap the signature in a MessageWrapper with an invalid namespace
+			swn := sign.WrappedSig{
+				Version:       1,
+				PublicKey:     string(tt.pub),
+				Namespace:     "file",
+				HashAlgorithm: sign.DefaultHashAlgorithm,
+				Signature:     string(ssh.Marshal(sig)),
+			}
+		
+			copy(swn.MagicHeader[:], sign.MagicHeader)
+		
+			swnPEM := pem.EncodeToMemory(&pem.Block{
+				Type:  "SSH SIGNATURE",
+				Bytes: ssh.Marshal(sig),
+			})
+
+			// Check that Decode returns an error when given an invalid 
+			// signature
+			_, err = Decode(swnPEM)
+			if err == nil {
+				t.Fatalf("Decode returned no error, expected error")
+			}
+
+			// Wrap the signature in a MessageWrapper with an invalid hash
+			swh := sign.WrappedSig{
+				Version:       1,
+				PublicKey:     string(tt.pub),
+				Namespace:     sign.Namespace,
+				HashAlgorithm: "invalid",
+				Signature:     string(ssh.Marshal(sig)),
+			}
+
+			copy(swh.MagicHeader[:], sign.MagicHeader)
+		
+			swhPEM := pem.EncodeToMemory(&pem.Block{
+				Type:  "SSH SIGNATURE",
+				Bytes: ssh.Marshal(sig),
+			})
+			_, err = Decode(swhPEM)
+			if err == nil {
+				t.Fatalf("Decode returned no error, expected error")
+			}
+
+			// Wrap the signature in a MessageWrapper with an invalid magic 
+			// header
+			swmh := sign.WrappedSig{
+				Version:       1,
+				PublicKey:     string(tt.pub),
+				Namespace:     sign.Namespace,
+				HashAlgorithm: sign.DefaultHashAlgorithm,
+				Signature:     string(ssh.Marshal(sig)),
+			}
+
+			copy(swmh.MagicHeader[:], "INVALID")
+		
+			swmhPEM := pem.EncodeToMemory(&pem.Block{
+				Type:  "SSH SIGNATURE",
+				Bytes: ssh.Marshal(sig),
+			})
+			_, err = Decode(swmhPEM)
+			if err == nil {
+				t.Fatalf("Decode returned no error, expected error")
+			}
+
+			// Wrap the signature in a MessageWrapper with an invalid version
+			swv := sign.WrappedSig{
+				Version:       1000,
+				PublicKey:     string(tt.pub),
+				Namespace:     sign.Namespace,
+				HashAlgorithm: sign.DefaultHashAlgorithm,
+				Signature:     string(ssh.Marshal(sig)),
+			}
+
+			copy(swv.MagicHeader[:], sign.MagicHeader)
+		
+			swvPEM := pem.EncodeToMemory(&pem.Block{
+				Type:  "SSH SIGNATURE",
+				Bytes: ssh.Marshal(sig),
+			})
+			_, err = Decode(swvPEM)
+			if err == nil {
+				t.Fatalf("Decode returned no error, expected error")
+			}
+
+
 		})
 	}
 
